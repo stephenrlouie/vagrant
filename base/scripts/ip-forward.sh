3eb8c9a58c8dd258e48ac62603c912b5dbfb09f6
Steve Louie <stelouie@cisco.com>
2018-04-10 15:45:11 -0400

    deploying the tiller service

---
+++ base/scripts/ip-forward.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo "net.ipv4.ip_forward = 1" >> /usr/lib/sysctl.d/50-default.conf
--- scripts/deploy-helm.sh
+++ scripts/deploy-helm.sh
@@ -5,6 +5,7 @@ sudo su
 
 # create a persistent volume
 kubectl create -f /home/vagrant/pv1.yaml
+kubectl create -f /home/vagrant/tiller.yaml
 kubectl create clusterrolebinding add-on-cluster-admin --clusterrole=cluster-admin --serviceaccount=kube-system:default
 
 # use helm client CLI to deploy helm tiller onto this cluster
