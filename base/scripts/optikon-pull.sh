244e3ccbce7e62e316f10f139d94b4673ea83545
Steve Louie <stelouie@cisco.com>
2018-05-02 11:01:48 +0200

    combine the optikon pull script with all pull

--- base/scripts/optikon-pull.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Optikon docker image pull
-docker pull dockerhub.cisco.com/intelligent-edge-dev-docker-local/optikon-ui:0.1
-docker pull dockerhub.cisco.com/intelligent-edge-dev-docker-local/optikon-api:0.1
--- base/scripts/pull-images.sh
+++ base/scripts/pull-images.sh
@@ -9,3 +9,5 @@ docker pull k8s.gcr.io/kube-state-metrics:v1.2.0
 docker pull prom/node-exporter:v0.15.2
 docker pull prom/prometheus:v2.2.1
 docker pull prom/pushgateway:v0.4.0
+docker pull dockerhub.cisco.com/intelligent-edge-dev-docker-local/optikon-ui:0.1
+docker pull dockerhub.cisco.com/intelligent-edge-dev-docker-local/optikon-api:0.1
