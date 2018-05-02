2125f48c4fc5afcab558daee0a743507e0a2d0de
Steve Louie <stelouie@cisco.com>
2018-05-02 10:31:34 +0200

    script to pull required images

---
+++ base/scripts/pull-images.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+docker pull gcr.io/crreleases/clusterregistry:latest
+docker pull k8s.gcr.io/etcd:3.0.17
+docker pull nginx:1.7.9
+docker pull prom/alertmanager:v0.14.0
+docker pull jimmidyson/configmap-reload:v0.1
+docker pull busybox:latest
+docker pull k8s.gcr.io/kube-state-metrics:v1.2.0
+docker pull prom/node-exporter:v0.15.2
+docker pull prom/prometheus:v2.2.1
+docker pull prom/pushgateway:v0.4.0
